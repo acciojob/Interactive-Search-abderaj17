b4b68f3d85e28bae757609c59cd881bef7f8aaa1
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-04-06 22:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const searchDiv = document.querySelector('.search');
+const searchInput = document.querySelector('.input');
+const searchBtn = document.querySelector('.btn');
+
+searchBtn.addEventListener('click', () => {
+    searchDiv.classList.add('active');
+    searchInput.focus();
+});
+
+// Remove active class when input is not focused
+searchInput.addEventListener('blur', () => {
+    searchDiv.classList.remove('active');
+});
